1bfdda430c1923770add848470cff384264dcb85
Clayton Zambon <zambon@outlook.com.br>
2024-09-15 15:04:40 -0300

    FEAT - Exercicio_basico - Concatenar Char com String

---
+++ src/a001_exerciciosbasicos/concatenarCharComString/ConcatenarCharComString.java
@@ -0,0 +1,16 @@
+package a001_exerciciosbasicos.concatenarCharComString;
+
+import javax.swing.JOptionPane;
+
+public class ConcatenarCharComString {
+
+	public static void main(String[] args) {
+		
+		char inicial = 'H';
+		String resto = "aumea";
+		
+		JOptionPane.showMessageDialog(null, inicial + resto);
+
+	}
+
+}
