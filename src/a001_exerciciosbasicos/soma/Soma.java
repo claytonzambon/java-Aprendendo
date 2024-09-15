dbe2560a7381de3c3c03d2498fe976ed59b96649
Clayton Zambon <zambon@outlook.com.br>
2024-09-15 14:48:42 -0300

    FEAT - Exercicio_basico - Soma

---
+++ src/a001_exerciciosbasicos/soma/Soma.java
@@ -0,0 +1,14 @@
+package a001_exerciciosbasicos.soma;
+
+import javax.swing.JOptionPane;
+
+public class Soma {
+
+	public static void main(String[] args) {
+
+		int resultado = 10+5;
+		JOptionPane.showMessageDialog(null, "10 + 5 = " + resultado);
+		
+	}
+
+}
