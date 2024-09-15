832ee8497e730cb20ff57fd42024acb05cf44554
Clayton Zambon <zambon@outlook.com.br>
2024-09-15 14:51:22 -0300

    FEAT - Exercicios_basicos - Subtracao

---
+++ src/a001_exerciciosbasicos/subtracao/Subtracao.java
@@ -0,0 +1,14 @@
+package a001_exerciciosbasicos.subtracao;
+
+import javax.swing.JOptionPane;
+
+public class Subtracao {
+
+	public static void main(String[] args) {
+
+		int resultado = 10-5;
+		JOptionPane.showMessageDialog(null, "10 - 5 = " + resultado);
+
+	}
+
+}
